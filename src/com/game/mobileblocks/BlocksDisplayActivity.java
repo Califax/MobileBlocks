4e78e13092e4965cd325a7731fa9dde58aebb864
farzonl <farzonl@gmail.com>
2013-04-19 19:37:57 -0400

    fixed project properties

--- src/com/game/mobileblocks/BlocksDisplayActivity.java
+++ src/com/game/mobileblocks/BlocksDisplayActivity.java
@@ -21,7 +21,7 @@ public class BlocksDisplayActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.blocks_display, menu);
+		//getMenuInflater().inflate(R.menu.blocks_display, menu);
 		return true;
 	}
 
