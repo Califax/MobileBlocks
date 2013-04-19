d584ef736583b871d20b26641231baa6dec9d192
farzonl <farzonl@gmail.com>
2013-04-19 19:22:10 -0400

    start of project

---
+++ src/com/game/mobileblocks/BlocksDisplayActivity.java
@@ -0,0 +1,28 @@
+package com.game.mobileblocks;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.Menu;
+
+public class BlocksDisplayActivity extends Activity {
+	DrawView drawView;
+	
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+
+	        drawView = new DrawView(this);
+	        drawView.setBackgroundColor(Color.WHITE);
+	        setContentView(drawView);
+
+	    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.blocks_display, menu);
+		return true;
+	}
+
+}
