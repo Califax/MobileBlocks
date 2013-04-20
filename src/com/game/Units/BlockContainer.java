793eee84b283512fa5f4b547623ae40a5fcd9584
Califax <NoQuarterKJ@gmail.com>
2013-04-19 23:03:02 -0400

    added blocks

---
+++ src/com/game/Units/BlockContainer.java
@@ -0,0 +1,8 @@
+package com.game.Units;
+
+public class BlockContainer {
+	private int x;
+	private int y;
+	private int width;
+	private int color;
+}
--- src/com/game/mobileblocks/BlocksDisplayActivity.java
+++ src/com/game/mobileblocks/BlocksDisplayActivity.java
@@ -9,40 +9,31 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class BlocksDisplayActivity extends Activity {
-	//DrawView drawView;
-	
-	    /*@Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
 
-	        drawView = new DrawView(this);
-	        drawView.setBackgroundColor(Color.WHITE);
-	        setContentView(drawView);
+	private String TAG = "BlocksDisplayActivity";
 
-	    }*/
-	    String TAG = "BlocksDisplayActivity";
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        // requesting to turn the title OFF
-	        requestWindowFeature(Window.FEATURE_NO_TITLE);
-	        // making it full screen
-	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	        // set our MainGamePanel as the View
-	        setContentView(new GamePanel(this));
-	        Log.d(TAG, "View added");
-	    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// requesting to turn the title OFF
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// making it full screen
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// set our MainGamePanel as the View
+		setContentView(new GamePanel(this));
+		Log.d(TAG, "View added");
+	}
 
-		@Override
-		protected void onDestroy() {
-			Log.d(TAG, "Destroying...");
-			super.onDestroy();
-		}
+	@Override
+	protected void onDestroy() {
+		Log.d(TAG, "Destroying...");
+		super.onDestroy();
+	}
 
-		@Override
-		protected void onStop() {
-			Log.d(TAG, "Stopping...");
-			super.onStop();
-		}
+	@Override
+	protected void onStop() {
+		Log.d(TAG, "Stopping...");
+		super.onStop();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
